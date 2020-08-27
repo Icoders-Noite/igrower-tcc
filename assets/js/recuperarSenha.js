265d2d6bb7d480b464d08164f8a8fbc973d68b33
Guilherme Klesse de Miranda <guiklesse@gmail.com>
2020-08-26 22:30:16 -0300

    tentativa de classe generica para requests não deu

--- assets/js/recuperarSenha.js
+++ assets/js/recuperarSenha.js
@@ -1,7 +1,1 @@
-let api=new ApiRequester("https://cors-anywhere.herokuapp.com/https://apiproducts.rj.r.appspot.com")
-$("#teste").click(function(){
-
-let a = api.get("/products","")
-console.log(a)
-})
 
