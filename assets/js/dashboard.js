314f561213725e0d1bf5a357ecbbc5ff08e12996
Oshirolmo <lucas.oshirolmo@gmail.com>
2020-10-12 15:41:57 -0300

    Att Graficos 12/10 Dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,8 +3,10 @@ $(window).on('load', function () {
     $('#preloader .inner').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
     $('body').delay(350).css({ 'overflow': 'visible' });
-   
-    chartDough(80,'graficos-dashboard','Umidade - Solo')
+
+    chartDough(80, 'graficos-solo-dashboard', 'Umidade - Solo', 'Solo');
+    chartDough(50, 'graficos-ar-dashboard', 'Umidade - Ar', 'Ar');
+    chartTemperatureDough(33, 'graficos-temperatura-dashboard')
 })
 //]]>
 
@@ -16,16 +18,22 @@ $('#abrirConfig').click(function () {
     loadConfig()
 });
 
-function chartDough(data,element,label) {
+function chartDough(data, element, label, tipo) {
 
     var ctx = document.getElementById(element).getContext('2d');
-    var corUmidade = switchCorUmidadeAr(data)
+    if (tipo == 'Solo') {
+        var corUmidade = switchCorUmidadeSolo(data)
+    } else {
+        var corUmidade = switchCorUmidadeAr(data)
+    }
     var corResto = 'rgba(242, 242, 242,0.2)'
     var resto = 100 - data
     var options = {
+        responsive: true,
+        maintainAspectRatio: false,
         elements: {
             center: {
-                text: `Umidade Solo: ${data}%`,
+                text: `Umidade ${tipo}: ${data}%`,
                 color: corUmidade, // Default is #000000
                 fontStyle: 'Arial', // Default is Arial
                 sidePadding: 20, // Default is 20 (as a percentage)
@@ -40,9 +48,9 @@ function chartDough(data,element,label) {
     var dataSolo = {
         labels: [label],
         datasets: [{
-            data: [data,resto],
+            data: [data, resto],
             backgroundColor: [
-                 corUmidade,corResto
+                corUmidade, corResto
 
             ],
             borderColor: [
@@ -145,20 +153,83 @@ function chartDough(data,element,label) {
             }
         }
     });
- 
-    myChart.update(); 
+
+    myChart.update();
 }
+
+function chartTemperatureDough(data, element) {
+    var bar_ctx = document.getElementById(element);
+    var bar_chart = new Chart(bar_ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: [],
+            datasets: [{
+                data: [data],
+                backgroundColor: switchCorTemperatura(data),
+                datalabels: {
+                    color: 'white'                    		//Color for percentage value
+                }
+            }, {
+                data: [45 - data],
+                backgroundColor: "lightgrey",
+                hoverBackgroundColor: "lightgrey",
+                datalabels: {
+                    color: 'lightgray' 									// Make the color of the second bar percentage value same as the color of the bar
+                }
+            },]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            tooltips: {
+                enabled: false
+            },
+            scales: {
+                xAxes: [{
+                    display: false,
+                    stacked: true
+                }],
+                yAxes: [{
+                    display: false,
+                    stacked: true
+                }],
+            }, // scales
+            plugins: {																	// PROVIDE PLUGINS where you can specify custom style
+                datalabels: {
+                    align: "start",
+                    anchor: "end",
+                    backgroundColor: null,
+                    borderColor: null,
+                    borderRadius: 4,
+                    borderWidth: 1,
+                    font: {
+                        size: 20,
+                        weight: "bold",											//Provide Font family for fancier look
+                    },
+                    offset: 10,
+                    formatter: function (value, context) {
+                        return context.chart.data.labels[context.dataIndex];		//Provide value of the percentage manually or through data
+                    },
+                },
+            },
+        }, // options
+    });
+}
+
 function switchCorUmidadeAr(data) {
 
     if (data <= 40) {
-        
+
         return 'rgba(252,30,68,0.8)'
     } else {
         if (data > 40 && data < 50) {
-            return 'rgba(255, 237, 71,0.8)'    
-          
+            return 'rgba(255, 237, 71,0.8)'
+
         } else {
-  
+
             return 'rgba(90, 255, 71,0.8)'
         }
     }
@@ -167,17 +238,23 @@ function switchCorUmidadeAr(data) {
 
 function switchCorUmidadeSolo(data) {
 
-    if (data <= 40) {
-        
-        return 'rgba(252,30,68,0.8)'
+    if (data <= 10) {
+
+        return 'rgba(199,80,107,0.8)'
     } else {
-        if (data > 40 && data < 50) {
-            return 'rgba(255, 237, 71,0.8)'    
-          
-        } else {
-  
-            return 'rgba(90, 255, 71,0.8)'
-        }
+        return 'rgba(0, 132, 184,0.8)'
     }
 
+}
+
+function switchCorTemperatura(data) {
+    if (data <= 12) {
+        return 'rgba(0,0,255,0.8)'
+    } else {
+        if (data > 12 && data < 24) {
+            return 'rgba(0,255,0,0.8)'
+        } else {
+            return 'rgba(255,0,0,0.8)'
+        }
+    }
 }
